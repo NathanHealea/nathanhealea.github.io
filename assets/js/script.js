49083c3640f17a68df968d6b06cbe897968b7d92
NathanHealea <nate.healea@gmail.com>
2017-05-05 02:39:13 -0700

    Navbar styled, Intro section being worked on

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+	// On document ready
+	$(document).ready(function(){
+
+
+		// Simple Text Rotator
+		$(".rotate").textrotator({
+			animation: "fade", 
+			separator: " | ", 
+			speed: 3750 
+		});
+
+	});
+
+});
