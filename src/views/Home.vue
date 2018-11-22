5f30daf6df20b588c830fa1ce972804942d0166c
NathanHealea <nate.healea@gmail.com>
2018-11-22 14:16:00 -0800

    vue project init

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <secion id="home">
+    <h1>Hello World </h1>
+    test
+  </secion>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
