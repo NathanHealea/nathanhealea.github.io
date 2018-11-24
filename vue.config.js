c9cde53fe2509e93a1373813f06f68b881ffc182
NathanHealea <nate.healea@gmail.com>
2018-11-24 08:47:19 -0800

    changed out put of index for delopyment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 // vue.config.js
 module.exports = {
+    baseUrl : process.env.NODE_ENV === 'production'
+        ? '/dist/'
+        : '/',
+    
     chainWebpack: config => {
       if(config.plugins.has('extract-css')) {
         const extractCSSPlugin = config.plugin('extract-css')
