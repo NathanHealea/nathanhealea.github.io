ad5e657f78ec12ccae81ebb72e0922a79cdc3cef
nathanhealea <nate.healea@gmail.com>
2019-01-26 10:00:26 -0800

    added temp profile picture

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,14 +3,14 @@
     <h1>About Me</h1>
     <div class="row">
         <div class="col-3 justify-content-center">
-            <img src="" alt="Nathan Healea Profile Picture" />
+            <img src="@/assets/nathan-healea-profile-picture.jpeg" alt="Nathan Healea Profile Picture" class="img-fluid"/>
         </div>
         <div class="col">
-            <p>Here is some text about by my self that im using as a place holder to show what this would look like when I get around to actualy writing an about section</p>
+            <p>Bacon ipsum dolor amet jowl fatback hamburger venison pastrami pork loin kielbasa buffalo flank bacon rump pork chop. Picanha shoulder alcatra hamburger chuck biltong salami corned beef capicola meatloaf porchetta pork chop jerky pork belly. Strip steak doner swine spare ribs ham rump leberkas capicola tongue salami fatback pig cow shoulder flank. Pork chop beef ribs spare ribs, corned beef tongue hamburger shank bresaola. Tongue short loin kevin pig. Kevin brisket cow bresaola. Tongue shoulder beef ribs ham ribeye ham hock, pancetta frankfurter doner pork chop sausage short ribs andouille.</p>
 
         </div>
     </div>
-    <div id="social-media" class="row">
+    <div id="social-media" class="row text-center my-3">
         <!-- GitHub !-->
         <div class="col">
             <a href="https://github.com/NathanHealea"><span class="fa-stack fa-1x"><i class="far fa-circle fa-stack-2x"></i><i class="fab fa-github fa-stack-1x"></i></span></a>
@@ -39,7 +39,8 @@
         <div class="col">
             <a href="https://twitter.com/nathanhealea"><span class="fa-stack fa-1x"><i class="far fa-circle fa-stack-2x"></i><i class="fab fa-twitter fa-stack-1x"></i>   </span></a>
         </div>
-        <!-- <div class="col"><a href="https://www.behance.net/nathanhealea"><i class="fas fa-behance fa-2x circle-icon" title="Behance"></i></a></div> --></div>
+        <!-- <div class="col"><a href="https://www.behance.net/nathanhealea"><i class="fas fa-behance fa-2x circle-icon" title="Behance"></i></a></div> -->
+    </div>
 </section>
 </template>
 
@@ -48,8 +49,3 @@ export default {
 
 }
 </script>
-
-<style>
-
-</style>
-
