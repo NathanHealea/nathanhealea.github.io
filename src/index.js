3a75254f1ca4998e05b63b18611319932f1b34af
nathanhealea <nate.healea@gmail.com>
2019-03-16 08:33:43 -0700

    Working modals

--- src/index.js
+++ src/index.js
@@ -8,4 +8,25 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(far, fab, fas);
 dom.watch();
 
+var btnModal = document.querySelectorAll('[data-toggle="modal"]');
+
+[].forEach.call(btnModal, function(btn){
+    btn.addEventListener('click', function(){
+
+        var modal = document.querySelector('#' + this.dataset.target);
+        var html = document.querySelector('html');
+
+        console.log(modal);
+        console.log(html);
+
+        modal.classList.add('is-active');
+        html.classList.add('is-clipped');
+
+        modal.querySelector('.modal-close').addEventListener('click', function(e){
+            console.log('button clicked')
+            modal.classList.remove('is-active');
+            html.classList.remove('is-clipped');
+        })
+    })
+})
 
