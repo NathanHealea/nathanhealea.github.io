5cfc5bc242e156a26b2583b8d7db14da4ee99402
nathanhealea <nate.healea@gmail.com>
2019-03-17 09:00:48 -0700

    Finished project section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// styling import
+// -- Styling import -- //
 require('./styles/main.scss');
 
+// -- Fontawesome -- //
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -8,25 +9,22 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(far, fab, fas);
 dom.watch();
 
-var btnModal = document.querySelectorAll('[data-toggle="modal"]');
 
+// -- Modals -- //
+var btnModal = document.querySelectorAll('[data-toggle="modal"]');
 [].forEach.call(btnModal, function(btn){
     btn.addEventListener('click', function(){
 
         var modal = document.querySelector('#' + this.dataset.target);
         var html = document.querySelector('html');
 
-        console.log(modal);
-        console.log(html);
-
         modal.classList.add('is-active');
         html.classList.add('is-clipped');
 
         modal.querySelector('.modal-close').addEventListener('click', function(e){
-            console.log('button clicked')
             modal.classList.remove('is-active');
             html.classList.remove('is-clipped');
-        })
-    })
-})
+        });
+    });
+});
 
