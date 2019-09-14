10866b40505c949ed0ac9a7f5f1d9162c2de5008
Nathan Healea <nate.healea@gmail.com>
2019-09-14 11:51:32 -0700

    Update the about me section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,31 +1,35 @@
 // --- Imports --- //
-import React from 'react'
+import React from 'react';
 
 // --- Material Ui Imports  --- //
-import Box from '@material-ui/core/Box'
+import Box from '@material-ui/core/Box';
 
 // --- Custom Components Imports --- //
-import { Section, Title, Subtitle } from '../../components'
+import { Section, Title, Subtitle } from '../../components';
 
 // --- Colors --- //
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
-
 function About() {
-
   return (
     <Section>
       <Container align="center">
         <Title variant="h3">Hello World; I'm Nathan Healea </Title>
         <Subtitle variant="h5">
-          Since beginning my joureny as a full stack engineer, I'v developed databases and parsers for research grant funded projects, REST APIs for detecting proper english, web application to simplify processes and track engagements, maintained databases aggergation scripts, intergration multiple third party, and much more. I'm always learn to expand my knowledge and look for intresting problems to solve with skills.
+          Since my journey as a software engineer, I’ve joined organization to
+          work with clients to define project scope and gather project
+          requirements, architecte software infrastructure, develop high quality
+          application to simplify and improve business processes, write code
+          that is maintainable, clean, and well documented, worked with
+          development team create software development standards. I’m always
+          learning to expand my knowledge, looking for interesting problems to
+          solve me with my skill.
         </Subtitle>
       </Container>
     </Section>
-
-  )
+  );
 }
 
 // --- Exports --- //
-export default About
+export default About;
